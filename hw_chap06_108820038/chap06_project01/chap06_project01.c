6fe7460f75f0f6faf9462fe8a1afb10ed4a55625
txya900619 <wayne900619@gmail.com>
2019-10-12 12:50:05 +0800

    change %g to atof method

--- hw_chap06_108820038/chap06_project01/chap06_project01.c
+++ hw_chap06_108820038/chap06_project01/chap06_project01.c
@@ -4,24 +4,27 @@
 /* ID: 108820038 (your student ID here)                          */
 /* Date: 2019.10.10 (put program development started date here   */
 /* Purpose: 找最大值                                              */
-/* Change History: 2019.10.10初打                                */
+/* Change History: 2019.10.12改atof                               */
 /*****************************************************************/
 #include <stdio.h>
+#include<stdlib.h>
+#include<string.h>
 int main(void){
-    float ans=0,tmp;//宣告變數
-    
+    char ans[60], input[60]; //宣告變數
+    double inputNum;
     while (1){
         printf("Enter a number:");
-        scanf("%f", &tmp);
-        if(tmp<=0){
+        scanf("%s", input);
+        inputNum = atof(input);
+        if(inputNum<=0){
             break;
-        }else if(ans<tmp){
-            ans = tmp;
+        }else if(atof(ans)<inputNum){
+            strcpy(ans,input);
         }
 
     }//輸入並找出最大
     
-    printf("\nThe largest number entered was %g", ans);//輸出答案
+    printf("\nThe largest number entered was %s", ans);//輸出答案
 
     return 0;
 }
