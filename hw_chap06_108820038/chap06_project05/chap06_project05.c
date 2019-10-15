b52f77cf3f39697c849dc2f6ed5e7b4d0e74df52
txya900619 <wayne900619@gmail.com>
2019-10-16 00:55:28 +0800

    complet chap 6

--- hw_chap06_108820038/chap06_project05/chap06_project05.c
+++ hw_chap06_108820038/chap06_project05/chap06_project05.c
@@ -3,11 +3,21 @@
 /* Author: 陳力瑋 (put your name here)                           */
 /* ID: 108820038 (your student ID here)                          */
 /* Date: 2019.10.10 (put program development started date here   */
-/* Purpose: 排序年月日                                            */
-/* Change History: 2019.10.10初打                                */
+/* Purpose: 翻轉數字                                             */
+/* Change History: 2019.10.15初打                                */
 /*****************************************************************/
 #include <stdio.h>
 int main(void){
+    int num;
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+    printf("Digits reversed: ");
+    
+    do{
+        printf("%d", num % 10);
+        num /= 10;
+    } while (num);
+
 
     return 0;
 }
--- hw_chap06_108820038/chap06_project11/chap06_project11.c
+++ hw_chap06_108820038/chap06_project11/chap06_project11.c
@@ -3,12 +3,16 @@
 /* Author: 陳力瑋 (put your name here)                           */
 /* ID: 108820038 (your student ID here)                          */
 /* Date: 2019.10.10 (put program development started date here   */
-/* Purpose: 算成績                                                */
-/* Change History: 2019.10.10初打                                */
+/* Purpose: 1*1/1!*1/2!....1/n!                                  */
+/* Change History: 2019.10.15初打                                */
 /*****************************************************************/
 #include <stdio.h>
 int main(void){
-    
-
+    float e=1,devi=1;
+    for (int i=2; i < 11;i++){
+        e += 1 / devi;
+        devi *= i;
+    }
+    printf("Approximation of e: %f", e);
     return 0;
 }
