a73be10287193fe3d4c1c1ab127bcf91a7a80d1f
txya900619 <wayne900619@gmail.com>
2019-11-17 22:53:59 +0800

    compelete chap10 project03

--- hw_chap10_108820038/chap10_project02/chap10_project02.c
+++ hw_chap10_108820038/chap10_project02/chap10_project02.c
@@ -8,147 +8,159 @@
 /*****************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
-int main(void) {
-    while (1) {
-        int straight = 0, flush = 0, four = 0, three = 0, pair = 0;
-        int rankNum[13], suitNum[4], cardReaded = 0, consecNum = 0;
-        int cardExist[13][4];
-        int rankFirst; //declare variable
 
-        for (int i = 0; i < 13; i++) {
-            for (int j = 0; j < 4; j++) {
-                if (i == 0) {
-                    suitNum[j] = 0;
-                }
-                cardExist[i][j] = 0;
-            }
-            rankNum[i] = 0;
-        } //init variable
+int straight = 0, flush = 0, four = 0, three = 0, pair = 0;
 
-        while (cardReaded < 5) {
-            char c;
-            int badCard = 0, rank, suit;
-            printf("Enter a card! : ");
-            c = getchar();
-            if (c == 48) {
-                exit(0);
-            } else if (c > 49 && c < 58) {
-                rank = c - '2';
-            } else {
-                switch (c) {
-                    case 't':
-                    case 'T':
-                        rank = 8;
-                        break;
-                    case 'j':
-                    case 'J':
-                        rank = 9;
-                        break;
-                    case 'q':
-                    case 'Q':
-                        rank = 10;
-                        break;
-                    case 'k':
-                    case 'K':
-                        rank = 11;
-                        break;
-                    case 'a':
-                    case 'A':
-                        rank = 12;
-                        break;
-                    default:
-                        badCard = 1;
-                }
+int cardExist[13][4];
+int rankFirst; //declare variable
+
+void read_cards(int rankNum[], int suitNum[]) {
+    int cardReaded = 0;
+    for (int i = 0; i < 13; i++) {
+        for (int j = 0; j < 4; j++) {
+            if (i == 0) {
+                suitNum[j] = 0;
             }
-            c = getchar();
+            cardExist[i][j] = 0;
+        }
+        rankNum[i] = 0;
+    } //init variable
+
+    while (cardReaded < 5) {
+        char c;
+        int badCard = 0, rank, suit;
+        printf("Enter a card! : ");
+        c = getchar();
+        if (c == 48) {
+            exit(0);
+        } else if (c > 49 && c < 58) {
+            rank = c - '2';
+        } else {
             switch (c) {
-                case 'c':
-                case 'C':
-                    suit = 0;
+                case 't':
+                case 'T':
+                    rank = 8;
                     break;
-                case 'd':
-                case 'D':
-                    suit = 1;
+                case 'j':
+                case 'J':
+                    rank = 9;
                     break;
-                case 'h':
-                case 'H':
-                    suit = 2;
+                case 'q':
+                case 'Q':
+                    rank = 10;
                     break;
-                case 's':
-                case 'S':
-                    suit = 3;
+                case 'k':
+                case 'K':
+                    rank = 11;
+                    break;
+                case 'a':
+                case 'A':
+                    rank = 12;
                     break;
                 default:
                     badCard = 1;
             }
-            while ((c = getchar()) != '\n') {
-                if (c != ' ') {
-                    badCard = 1;
-                }
-            }
-            if (badCard) {
-                printf("Bad card; ignored. \n");
-            } else if (cardExist[rank][suit] != 0) {
-                printf("Duplicate card; ignored. \n");
-            } else {
-                suitNum[suit]++;
-                rankNum[rank]++;
-                cardExist[rank][suit]++;
-                cardReaded++;
-            } //input
         }
-
-        for (int i = 0; i < 4; i++) {
-            if (suitNum[i] == 5) {
-                flush = 1;
-            }
-        } // check flush
-
-        rankFirst = 0;
-        while (rankNum[rankFirst] == 0) {
-            rankFirst++;
-        }
-        for (; rankFirst < 13 && rankNum[rankFirst] > 0; rankFirst++) {
-            consecNum++;
-        }
-        if (consecNum == 5) {
-            straight = 1;
-        } //check straight
-
-        for (int i = 0; i < 13; i++) {
-            if (rankNum[i] == 4) {
-                four = 1;
+        c = getchar();
+        switch (c) {
+            case 'c':
+            case 'C':
+                suit = 0;
                 break;
-            }
-            if (rankNum[i] == 3) {
-                three = 1;
-                continue;
-            }
-            if (rankNum[i] == 2) {
-                pair++;
-            }
-        } //check pair three of king.....
-
-        if (straight == 1 && flush == 1) {
-            printf("Straight flush");
-        } else if (four == 1) {
-            printf("Four of a king");
-        } else if (three == 1 && pair == 1) {
-            printf("Full house");
-        } else if (flush == 1) {
-            printf("Flush");
-        } else if (straight == 1) {
-            printf("Straight");
-        } else if (three == 1) {
-            printf("Three of a king");
-        } else if (pair == 2) {
-            printf("Two pair");
-        } else if (pair == 1) {
-            printf("Pair");
-        } else {
-            printf("High card");
+            case 'd':
+            case 'D':
+                suit = 1;
+                break;
+            case 'h':
+            case 'H':
+                suit = 2;
+                break;
+            case 's':
+            case 'S':
+                suit = 3;
+                break;
+            default:
+                badCard = 1;
         }
-        printf("\n\n"); //output
+        while ((c = getchar()) != '\n') {
+            if (c != ' ') {
+                badCard = 1;
+            }
+        }
+        if (badCard) {
+            printf("Bad card; ignored. \n");
+        } else if (cardExist[rank][suit] != 0) {
+            printf("Duplicate card; ignored. \n");
+        } else {
+            suitNum[suit]++;
+            rankNum[rank]++;
+            cardExist[rank][suit]++;
+            cardReaded++;
+        } //input
+    }
+}
+void analyze_hand(int rankNum[], int suitNum[]) {
+    int consecNum = 0;
+    straight = 0, flush = 0, four = 0, three = 0, pair = 0;
+    for (int i = 0; i < 4; i++) {
+        if (suitNum[i] == 5) {
+            flush = 1;
+        }
+    } // check flush
+
+    rankFirst = 0;
+    while (rankNum[rankFirst] == 0) {
+        rankFirst++;
+    }
+    for (; rankFirst < 13 && rankNum[rankFirst] > 0; rankFirst++) {
+        consecNum++;
+    }
+    if (consecNum == 5) {
+        straight = 1;
+    } //check straight
+
+    for (int i = 0; i < 13; i++) {
+        if (rankNum[i] == 4) {
+            four = 1;
+            break;
+        }
+        if (rankNum[i] == 3) {
+            three = 1;
+            continue;
+        }
+        if (rankNum[i] == 2) {
+            pair++;
+        }
+    } //check pair three of king.....
+}
+void print_result(void) {
+    if (straight == 1 && flush == 1) {
+        printf("Straight flush");
+    } else if (four == 1) {
+        printf("Four of a king");
+    } else if (three == 1 && pair == 1) {
+        printf("Full house");
+    } else if (flush == 1) {
+        printf("Flush");
+    } else if (straight == 1) {
+        printf("Straight");
+    } else if (three == 1) {
+        printf("Three of a king");
+    } else if (pair == 2) {
+        printf("Two pair");
+    } else if (pair == 1) {
+        printf("Pair");
+    } else {
+        printf("High card");
+    }
+    printf("\n\n"); //output
+}
+int main(void) {
+    int num_in_rank[13], num_in_suit[4]; //declare variable
+    while (1) {
+        read_cards(num_in_rank, num_in_suit);
+        analyze_hand(num_in_rank, num_in_suit);
+        print_result();
     }
     return 0;
 }
--- hw_chap10_108820038/chap10_project03/chap10_project03.c
+++ hw_chap10_108820038/chap10_project03/chap10_project03.c
@@ -2,34 +2,180 @@
 /* Class: Computer Programming, Fall 2019                        */
 /* Author: 陳力瑋 (put your name here)                           */
 /* ID: 108820038 (your student ID here)                          */
-/* Date: 2019.10.29 (put program development started date here   */
-/* Purpose: reverse sentence                                     */
-/* Change History: 2019.10.29初打                                */
+/* Date: 2019.11.7 (put program development started date here   */
+/* Purpose: poker   2                                            */
+/* Change History: 2019.11.17初打                                */
 /*****************************************************************/
 #include <stdio.h>
-int power(int x,int n){
-    int ans=0;
-    if(n==0){
-      return 1;   
+#include <stdlib.h>
+int straight = 0, flush = 0, four = 0, three = 0, pair = 0;
+int cardinhand[5][2]; //declare variable
+int duplicateCard(int rank, int suit, int cardinhand[5][2], int cardReaded) {
+    for (int i = 0; i < cardReaded; i++) {
+        if (cardinhand[i][0] == rank && cardinhand[i][1] == suit) {
+            return 1;
+        }
     }
-     ans = power(x, n / 2);
-    if(n%2==0){
-        return ans*ans;
+    return 0;
+}
+void read_card(void) {
+    char c;
+    int badCard, rank, suit, cardReaded = 0;
+    straight = 0, flush = 0, four = 0, three = 0, pair = 0; //declare variable
+    while (cardReaded < 5) {
+        badCard = 0;
+        printf("Enter a card! : ");
+        c = getchar();
+        if (c == 48) {
+            exit(0);
+        } else if (c > 49 && c < 58) {
+            rank = c - '2';
+        } else {
+            switch (c) {
+                case 't':
+                case 'T':
+                    rank = 8;
+                    break;
+                case 'j':
+                case 'J':
+                    rank = 9;
+                    break;
+                case 'q':
+                case 'Q':
+                    rank = 10;
+                    break;
+                case 'k':
+                case 'K':
+                    rank = 11;
+                    break;
+                case 'a':
+                case 'A':
+                    rank = 12;
+                    break;
+                default:
+                    badCard = 1;
+            }
+        }
+        c = getchar();
+        switch (c) {
+            case 'c':
+            case 'C':
+                suit = 0;
+                break;
+            case 'd':
+            case 'D':
+                suit = 1;
+                break;
+            case 'h':
+            case 'H':
+                suit = 2;
+                break;
+            case 's':
+            case 'S':
+                suit = 3;
+                break;
+            default:
+                badCard = 1;
+        }
+        while ((c = getchar()) != '\n') {
+            if (c != ' ') {
+                badCard = 1;
+            }
+        }
+        if (badCard) {
+            printf("Bad card; ignored. \n");
+        } else if (duplicateCard(rank, suit, cardinhand, cardReaded) == 1) {
+            printf("Duplicate card; ignored. \n");
+        } else {
+            cardinhand[cardReaded][0] = rank;
+            cardinhand[cardReaded][1] = suit;
+            cardReaded++;
+        } //input
     }
-    else{
-        return ans*ans* x;
+}
+void analyze_hand(void) {
+    int matches;
+    for (int i = 0; i < 5; i++) {
+        int tempSuit, tempRank, smallest = i;
+        for (int j = i + 1; j < 5; j++) {
+            if (cardinhand[j][0] < cardinhand[smallest][0]) {
+                smallest = j;
+            }
+        }
+        if (i != smallest) {
+            tempRank = cardinhand[i][0];
+            tempSuit = cardinhand[i][1];
+            cardinhand[i][0] = cardinhand[smallest][0];
+            cardinhand[i][1] = cardinhand[smallest][1];
+            cardinhand[smallest][0] = tempRank;
+            cardinhand[smallest][1] = tempSuit;
+        }
     }
-}//circulate
+    for (int i = 1; i < 5; i++) {
+        if (cardinhand[i][1] != cardinhand[0][1]) {
+            break;
+        }
+        if (i == 4) {
+            flush = 1;
+        }
+    } // check flush
 
-int main(void){
-    int x,n;//declare variable
+    for (int i = 1; i < 5; i++) {
+        if (cardinhand[i][0] - cardinhand[i - 1][0] != 1) {
+            break;
+        }
+        if (i == 4) {
+            straight = 1;
+        }
+    } //check straight
 
-    printf("For x^n, enter x: ");
-    scanf("%d",&x);
-    printf("Enter n: ");
-    scanf("%d",&n);//input x and n
-
-    printf("%d^%d = %d\n",x,n,power(x,n));//call function and output 
-    
+    for (int i = 0; i < 5; i++) {
+        matches = 0;
+        for (int j = i + 1; j < 5; j++) {
+            if (cardinhand[j][0] == cardinhand[i][0]) {
+                matches++;
+            }
+        }
+        if (matches == 1) {
+            pair++;
+        }
+        if (matches == 2) {
+            pair--;
+            three++;
+        }
+        if (matches == 3) {
+            four++;
+            break;
+        }
+    } //check pair three of king.....
+}
+void print_result(void) {
+    if (straight == 1 && flush == 1) {
+        printf("Straight flush");
+    } else if (four == 1) {
+        printf("Four of a king");
+    } else if (three == 1 && pair == 1) {
+        printf("Full house");
+    } else if (flush == 1) {
+        printf("Flush");
+    } else if (straight == 1) {
+        printf("Straight");
+    } else if (three == 1) {
+        printf("Three of a king");
+    } else if (pair == 2) {
+        printf("Two pair");
+    } else if (pair == 1) {
+        printf("Pair");
+    } else {
+        printf("High card");
+    }
+    printf("\n\n"); //output
+}
+int main(void) {
+    while (1) {
+        read_card();
+        analyze_hand();
+        print_result();
+    }
     return 0;
 }
