0be47a3d816e92f6f089832b622001736df656dc
txya900619 <wayne900619@gmail.com>
2019-11-17 23:51:43 +0800

    complete chap10 project 06

--- hw_chap10_108820038/chap10_project01/chap10_project01.c
+++ hw_chap10_108820038/chap10_project01/chap10_project01.c
@@ -7,56 +7,52 @@
 /* Change History: 2019.11.16 初打                               */
 /*****************************************************************/
 #include <stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
 
 #define MAX_STACK 100 //max stack
 
 char stack[MAX_STACK];
-int top = 0;//declare stack
+int top = -1; //declare stack
 
-void push (char c){
-    if(top==MAX_STACK){
+void push(char c) {
+    if (top == MAX_STACK) {
         printf("Stack overflow\n");
         exit(0);
     }
-    stack[top++]=c;
-}//push function
-char pop(){
-    if(top<=0){
+    stack[++top] = c;
+} //push function
+char pop() {
+    if (top < 0) {
         printf("Stack underflow\n");
         exit(0);
     }
-    return stack[--top];
-}//pop function
-int main(void){
+    return stack[top--];
+} //pop function
+int main(void) {
 
     int isPare = 1;
-    int hasInput=0;
-    char c;//declare variable
+    char c; //declare variable
 
     printf("Enter parentheses and/or braces :");
-    while( isPare&&(c=getchar())!='\n'){
-        if(c=='{'||c=='(' ){
+    while (isPare && (c = getchar()) != '\n') {
+        if (c == '{' || c == '(') {
             push(c);
-        }else if(c=='}'){
-            hasInput=1;
-            isPare=('{'==pop());
-        }else if(c==')'){
-            hasInput=1;
-            isPare=('('==pop());
-        }else{
+        } else if (c == '}') {
+            isPare = ('{' == pop());
+        } else if (c == ')') {
+            isPare = ('(' == pop());
+        } else {
             printf("please input {,(,},)\n");
             exit(0);
         }
-        
-    }//input and test
 
-    if(isPare&&hasInput){
+    } //input and test
+
+    if (isPare && top == -1) {
         printf("Parentheses/braces are nested properly\n");
-    }else{
+    } else {
         printf("Parentheses/braces NOT are nested properly\n");
-    }//output
-    
+    } //output
+
     return 0;
 }
-
--- hw_chap10_108820038/chap10_project06/chap10_project06.c
+++ hw_chap10_108820038/chap10_project06/chap10_project06.c
@@ -2,22 +2,71 @@
 /* Class: Computer Programming, Fall 2019                        */
 /* Author: 陳力瑋 (put your name here)                           */
 /* ID: 108820038 (your student ID here)                          */
-/* Date: 2019.10.19 (put program development started date here   */
-/* Purpose: 簡寫姓名                                             */
-/* Change History: 2019.10.19初打                                */
+/* Date: 2019.11.07 (put program development started date here   */
+/* Purpose: stack                                                */
+/* Change History: 2019.11.17 初打                               */
 /*****************************************************************/
 #include <stdio.h>
-#include<stdlib.h>
-int polynomial(int num){
-    return num*num*num*num*num*3+num*num*num*num*2-num*num*num*5-num*num+7*num-6;
-}//circulate
-int main(){
-    int input;//declare variable
+#include <stdlib.h>
 
-    printf("Enter an integer: ");
-    scanf("%d",&input);//input
+#define MAX_STACK 100 //max stack
 
-    printf("Result: %d\n",polynomial(input));//output
+int stack[MAX_STACK];
+int top = -1; //declare stack
+
+void push(int i) {
+    if (top == MAX_STACK) {
+        printf("Stack overflow\n");
+        exit(0);
+    }
+    stack[++top] = i;
+} //push function
+char pop() {
+    if (top < 0) {
+        printf("Stack underflow\n");
+        exit(0);
+    }
+    return stack[top--];
+} //pop function
+int main(void) {
+
+    char c;
+    int num, pop1, pop2; //declare variable
+
+    printf("Enter an RPN expression : ");
+    while (1) {
+        scanf(" %c", &c);
+        num = c - '0';
+        if (num > -1 && num < 10) {
+            push(num);
+        } else {
+            switch (c) {
+                case '+':
+                    push(pop() + pop());
+                    break;
+                case '-':
+                    pop1 = pop();
+                    pop2 = pop();
+                    push(pop2 - pop1);
+                    break;
+                case '*':
+                    push(pop() * pop());
+                    break;
+                case '/':
+                    pop1 = pop();
+                    pop2 = pop();
+                    push(pop2 / pop1);
+                    break;
+                case '=':
+                    printf("Value of expression : %d\n", pop());
+                    top = 0;
+                    printf("Enter an RPN expression : ");
+                    break;
+                default:
+                    exit(0);
+            }
+        }
+    } //input and calculate and output
 
     return 0;
 }
